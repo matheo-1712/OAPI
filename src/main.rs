df6c5631f0c6a9a816ecc7bc10357a3559b2117c
matheo-1712 <perod.matheo@gmail.com>
2026-05-20 01:44:28 +0200

    style: simplify log format with custom timestamp and compact style

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,14 @@ struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
-    // Initialize tracing
-    tracing_subscriber::registry()
-        .with(
+    // Initialize tracing with a simplified and compact format
+    tracing_subscriber::fmt()
+        .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| "info,OAPI=debug,tower_http=debug".into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with_timer(tracing_subscriber::fmt::time::ChronoLocal::new("%Y-%m-%d %H:%M:%S".to_string()))
+        .compact()
         .init();
 
     let banner = r#"
