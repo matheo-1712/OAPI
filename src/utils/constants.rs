94b9f73f9f09ad6f99af521dde013776e5e2ec5b
matheo-1712 <perod.matheo@gmail.com>
2026-05-21 04:19:54 +0200

    Export String to Constants file

---
+++ src/utils/constants.rs
@@ -0,0 +1,11 @@
+// ──────────────────────────────────────────────
+//  Discord Image
+// ──────────────────────────────────────────────
+pub const LABEL_MEMBER_SINCE: &str = "MEMBRE DEPUIS LE";
+pub const LABEL_MESSAGES: &str = "MESSAGES ENVOYÉS";
+pub const LABEL_VOCAL_TIME: &str = "TEMPS VOCAL TOTAL";
+pub const LABEL_COMPANION: &str = "MEILLEUR AMI LOUTRE";
+pub const LABEL_TEXT_CHANNEL: &str = "SALON TEXTUEL PRÉFÉRÉ";
+pub const LABEL_VOICE_CHANNEL: &str = "SALON VOCAL PRÉFÉRÉ";
+pub const LABEL_ROLES_TITLE: &str = "RÔLES LOUTRES";
+
